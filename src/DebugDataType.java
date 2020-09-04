24ac4d6aba1341a795b73438857a55f62d0cbc1a
ernidar <ernidar20@gmail.com>
2020-09-04 11:17:10 -0400

    Debug data type

---
+++ src/DebugDataType.java
@@ -0,0 +1,14 @@
+public class DebugDataType {
+    public static void main(String[] args) {
+
+        int number1=10;
+        int number2=5;
+        int number3=15;
+        int Sum,Product;
+
+        Product=number1*number2*number3;
+        Sum=number1+number1+number3;
+
+        System.out.println("Product : " + Product + "\n" + "Sum : " + Sum);
+    }
+}
